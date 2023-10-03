34ef81023930bbd0dd2e370bca67e6682479269b
hub <h.ramiandrasoa@rubika-edu.com>
2023-10-03 12:01:56 +0200

    TextMeshPro

--- TKTCaVole/Assets/Scripts/Generation/LevelGenerator.cs
+++ TKTCaVole/Assets/Scripts/Generation/LevelGenerator.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject ringPrefab;
+    [SerializeField] private GameObject rockPrefab;
+    
+    private void Start()
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
