6314bd5118e1e8fb26a603200320fb8d73a23f8a
CalioPur <a.marcelli@rubika-edu.com>
2023-10-03 16:47:34 +0200

    camera rotation fixed to ship rotation

--- TKTCaVole/Assets/Scripts/Pilotage/CameraControler.cs
+++ TKTCaVole/Assets/Scripts/Pilotage/CameraControler.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -31,14 +32,21 @@ public class CameraControler : MonoBehaviour
 
     void Update()
     {
-        transform.position = spaceShip.position;
+        transform.rotation = spaceShip.rotation;
         
-        
-        //_currentCameraRot = transform.parent.rotation.eulerAngles;
         _currentCameraRot.y += moveVector.x * MouseSensitivity;
+        // _currentCameraRot.y %= 360;
         _currentCameraRot.x += moveVector.y * MouseSensitivity;
+        //_currentCameraRot.x %= 360;
         
-        transform.rotation = Quaternion.Euler(_currentCameraRot);
+        transform.Rotate(_currentCameraRot);
+
+        
+        //print(_currentCameraRot);
+        //var rot = _currentCameraRot;
+        
+        //transform.rotation = Quaternion.Euler(_currentCameraRot);
+
 
     }
 }
