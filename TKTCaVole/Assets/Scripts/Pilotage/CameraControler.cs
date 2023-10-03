20d6c33455ce92fa1134072b6c3968a0c9fdd908
CalioPur <a.marcelli@rubika-edu.com>
2023-10-03 15:54:07 +0200

    new input system implemented

--- TKTCaVole/Assets/Scripts/Pilotage/CameraControler.cs
+++ TKTCaVole/Assets/Scripts/Pilotage/CameraControler.cs
@@ -1,26 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CameraControler : MonoBehaviour
 {
     [SerializeField] private Transform spaceShip;
-    private Vector3 _currentCameraRot = new Vector3(0, 0, -0);
+    private Vector2 moveVector = Vector2.zero;
 
-    // Update is called once per frame
+    private Vector3 _currentCameraRot = new Vector3(0, 0, -0);
+    [Range(0.1f,1f)]
+    public float MouseSensitivity;
+    private void Start()
+    {
+        GameInputManager.OnCameraPerformed += OnCamMovementPerformed;
+        GameInputManager.OnCameraCancelled += OnCamMovementCancelled;
+    }
+
+    
+
+    private void OnCamMovementPerformed(InputAction.CallbackContext ctx)
+    {
+        moveVector = ctx.ReadValue<Vector2>();
+    }
+    private void OnCamMovementCancelled(InputAction.CallbackContext ctx)
+    {
+        moveVector = Vector2.zero;
+
+    }
+
     void Update()
     {
         transform.position = spaceShip.position;
         
-        float mouseXMov = Input.GetAxis("Mouse X");
-        float mouseYMov = Input.GetAxis("Mouse Y");
+        
         //_currentCameraRot = transform.parent.rotation.eulerAngles;
-        _currentCameraRot.y += mouseXMov;
-        _currentCameraRot.x += mouseYMov;
+        _currentCameraRot.y += moveVector.x * MouseSensitivity;
+        _currentCameraRot.x += moveVector.y * MouseSensitivity;
         
         transform.rotation = Quaternion.Euler(_currentCameraRot);
-
-
 
     }
 }
