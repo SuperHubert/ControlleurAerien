d5f1be73e63974afc7c1565ea7fc6e5642a6b141
CalioPur <a.marcelli@rubika-edu.com>
2023-10-03 14:10:47 +0200

    base ShipController

--- TKTCaVole/Assets/Scripts/Pilotage/CameraControler.cs
+++ TKTCaVole/Assets/Scripts/Pilotage/CameraControler.cs
@@ -6,7 +6,6 @@ public class CameraControler : MonoBehaviour
 {
     [SerializeField] private Transform spaceShip;
     private Vector3 _currentCameraRot = new Vector3(0, 0, -0);
-    public int mouseSensitivity;
 
     // Update is called once per frame
     void Update()
@@ -15,10 +14,10 @@ public class CameraControler : MonoBehaviour
         
         float mouseXMov = Input.GetAxis("Mouse X");
         float mouseYMov = Input.GetAxis("Mouse Y");
-
-        _currentCameraRot.y += mouseXMov*mouseSensitivity;
-        _currentCameraRot.x += mouseYMov*mouseSensitivity;
-
+        //_currentCameraRot = transform.parent.rotation.eulerAngles;
+        _currentCameraRot.y += mouseXMov;
+        _currentCameraRot.x += mouseYMov;
+        
         transform.rotation = Quaternion.Euler(_currentCameraRot);
 
 
--- TKTCaVole/Assets/Scripts/Pilotage/ShipController.cs
+++ TKTCaVole/Assets/Scripts/Pilotage/ShipController.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Searcher;
 using UnityEngine;
 
 public class ShipController : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public float speed = 100.0F;
+    public int gear = 0;
+    public int gearValue = 20;
+    public float rotationSpeed = 75.0F;
     void Start()
     {
         
@@ -13,6 +18,18 @@ public class ShipController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float currentSpeed = speed + gear*gearValue;
         
+        float rotationSpeedVertical = Input.GetAxis("Vertical") * rotationSpeed * Time.deltaTime;
+        float rotationSpeedHorizontal = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
+        
+        transform.Translate(0, 0, currentSpeed * Time.deltaTime);
+        transform.Rotate(rotationSpeedVertical, rotationSpeedHorizontal, 0);
+
+        if (Input.GetKey(KeyCode.E) && gear < 5) gear++; //changer le FOV
+        if (Input.GetKey(KeyCode.A) && gear > 0) gear--;
+
+
     }
+
 }
