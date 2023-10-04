88f00e9bce641fe698b4376dfb660e7a96b7aef3
hub <h.ramiandrasoa@rubika-edu.com>
2023-10-04 11:07:24 +0200

    Rock script on rocks

--- TKTCaVole/Assets/Scripts/Rock.cs
+++ TKTCaVole/Assets/Scripts/Rock.cs
@@ -7,8 +7,9 @@ public class Rock : MonoBehaviour, IDamageable
     public bool DropsHourglass { get; private set; }
     private int health;
 
-    public bool SetRockData(bool drops,int hp)
+    public void SetRockData(float size,bool drops,int hp)
     {
+        transform.localScale = size * Vector3.one;
         DropsHourglass = drops;
         health = hp;
     }
