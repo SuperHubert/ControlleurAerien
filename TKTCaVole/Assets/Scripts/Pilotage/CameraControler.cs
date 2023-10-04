fee2f2278b1eca0ebb67b60cab9c84d1a7cec42a
CalioPur <a.marcelli@rubika-edu.com>
2023-10-04 14:43:32 +0200

    camera recenter
    middle mouseclick et click stick gauche

--- TKTCaVole/Assets/Scripts/Pilotage/CameraControler.cs
+++ TKTCaVole/Assets/Scripts/Pilotage/CameraControler.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -12,13 +13,25 @@ public class CameraControler : MonoBehaviour
     private Vector3 _currentCameraRot = new Vector3(0, 0, -0);
     
     public float MouseSensitivity;
+
+    private bool doCenter = false;
+    private float animDuration = 0.1f;
+    private float timeElapsed = .0f;
+    private Vector3 instantTCamPos;
+    
     private void Start()
     {
         GameInputManager.OnCameraPerformed += OnCamMovementPerformed;
         GameInputManager.OnCameraCancelled += OnCamMovementCancelled;
+        GameInputManager.OnCameraCenter += OnCameraCenter;
     }
 
-    
+    private void OnCameraCenter(InputAction.CallbackContext obj)
+    {
+        doCenter = true;
+        instantTCamPos = _currentCameraRot;
+    }
+
 
     private void OnCamMovementPerformed(InputAction.CallbackContext ctx)
     {
@@ -38,6 +51,21 @@ public class CameraControler : MonoBehaviour
         // _currentCameraRot.y %= 360;
         _currentCameraRot.x += moveVector.y * MouseSensitivity*Time.deltaTime;
         //_currentCameraRot.x %= 360;
+
+        if (doCenter)
+        {
+            if (timeElapsed < animDuration)
+            {
+                _currentCameraRot = Vector3.Lerp(instantTCamPos, Vector3.zero, timeElapsed / animDuration);
+                timeElapsed += Time.deltaTime;
+            }
+            else
+            {
+                timeElapsed = 0f;
+                doCenter = false;
+                _currentCameraRot = Vector3.zero;
+            }
+        }
         
         transform.Rotate(_currentCameraRot);
 
