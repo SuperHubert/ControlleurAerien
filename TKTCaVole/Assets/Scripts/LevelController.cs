f75b95544b2387bd21559f8fe705fe914b2baac1
esdragor <baudouinlapeze9@gmail.com>
2023-10-05 16:26:39 +0200

    remove debug log lock cursor

--- TKTCaVole/Assets/Scripts/LevelController.cs
+++ TKTCaVole/Assets/Scripts/LevelController.cs
@@ -43,7 +43,6 @@ public class LevelController : MonoBehaviour
     {
         shipGo.SetActive(true);
         Cursor.lockState = CursorLockMode.Locked;
-        Debug.Log("Locked Cursor");
 
         Gate.OnGatesLeftUpdatedTimer += IncreaseTimer;
         Gate.OnGatesLeftUpdated += TryWinLevel;
