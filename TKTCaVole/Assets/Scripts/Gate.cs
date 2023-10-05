cd68be380db78bf4d9ed5df7febd14a027203cfd
esdragor <baudouinlapeze9@gmail.com>
2023-10-05 11:58:30 +0200

    just dissolve en fait

--- TKTCaVole/Assets/Scripts/Gate.cs
+++ TKTCaVole/Assets/Scripts/Gate.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
 
@@ -6,25 +8,58 @@ public class Gate : MonoBehaviour
 {
     public static int GatesLeft { get; private set; }
     public static int TotalGates { get; private set; }
-    public static event Action<int,int> OnGatesLeftUpdated; 
+    public static event Action<int, int> OnGatesLeftUpdated;
     public static event Action<float> OnGatesLeftUpdatedTimer;
-    
+
     [SerializeField] private float increaseTimer = 15f;
     [SerializeField] private Animator animator;
-    [FormerlySerializedAs("collider")] [SerializeField] private Collider col;
+    [Header("Dissolve")]
+    [SerializeField] private List<GameObject> matDissolver;
+    [SerializeField] private float speedDissolve = 240;
+
+    [FormerlySerializedAs("collider")] [SerializeField]
+    private Collider col;
+
     private static readonly int DestroyTrigger = Animator.StringToHash("Destroy");
 
     public static void InitGates(int totalGates)
     {
         TotalGates = totalGates;
         GatesLeft = TotalGates;
-        
-        OnGatesLeftUpdated?.Invoke(GatesLeft,TotalGates);
+
+        OnGatesLeftUpdated?.Invoke(GatesLeft, TotalGates);
+    }
+
+    private void OnEnable()
+    {
+        for (int i = 0; i < matDissolver.Count; i++)
+        {
+            
+            matDissolver[i].GetComponent<Renderer>().material.SetFloat("_CutoffHeight", 130);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         TriggerGate();
+    }
+
+    private IEnumerator DissolveManagement()
+    {
+        bool Dissolve = false;
+
+        while (!Dissolve)
+        {
+            for (int i = 0; i < matDissolver.Count; i++)
+            {
+                matDissolver[i].GetComponent<Renderer>().material.SetFloat("_CutoffHeight",
+                    matDissolver[i].GetComponent<Renderer>().material.GetFloat("_CutoffHeight") - Time.deltaTime * speedDissolve);
+                if ( matDissolver[i].GetComponent<Renderer>().material.GetFloat("_CutoffHeight") <= -500.0f)
+                    Dissolve = true;
+            }
+
+            yield return new WaitForEndOfFrame();
+        }
     }
 
     [ContextMenu("Trigger")]
@@ -33,10 +68,11 @@ public class Gate : MonoBehaviour
         //TODO - Better Destroy;
         col.enabled = false;
         animator.SetTrigger(DestroyTrigger);
-        
+        StartCoroutine(DissolveManagement());
+
         GatesLeft--;
-        
-        OnGatesLeftUpdated?.Invoke(GatesLeft,TotalGates);
+
+        OnGatesLeftUpdated?.Invoke(GatesLeft, TotalGates);
         OnGatesLeftUpdatedTimer?.Invoke(increaseTimer);
     }
 }
